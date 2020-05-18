a2c2fe63a2cd5ae150071ceb46d0f267757cad99
cahillhowell <35302108+cahillhowell@users.noreply.github.com>
2020-05-18 15:37:35 -0400

    Added a comments file

---
+++ comments.java
@@ -0,0 +1,35 @@
+public class comments {
+	public static void main(String[] args) {
+	System.out.println("Go read the comments!");
+// will cancel out the line
+
+/*
+ * 
+ * 	Good way to block out a lot of lines
+ * 
+ *  List of different data types to use
+ * 	int myNum = 5;
+	float myFloatNum = 5.99f;
+	char myLetter = 'D';
+	boolean myBool = true;
+	String myText = "Hello";
+	  _
+ * 	 /-\
+ * 	 |||
+ *   |||
+ *  Copied from https://www.w3schools.com/java/java_variables.asp
+ *  Made a little arrow, neat
+ * 
+ * 
+ */
+
+//you can also block comment like this
+
+/*
+
+
+This works too, both are easy
+
+*/
+	}
+}
